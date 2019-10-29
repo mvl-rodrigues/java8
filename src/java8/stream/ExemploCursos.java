3a865dcbe836d904d9838934814f993e2861fc94
Victor Rodrigues <victor.rodrigues@metasix.com.br>
2019-10-29 10:50:10 -0300

    Aula5 - mais stream e optional

--- src/java8/stream/ExemploCursos.java
+++ src/java8/stream/ExemploCursos.java
@@ -2,6 +2,9 @@ package java8.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ExemploCursos {
 
@@ -14,7 +17,7 @@ public class ExemploCursos {
 		 */
 //		cursos.stream()
 //			.filter(c -> c.getAlunos() >= 100) //filter: a partir de um predicate filtra os objetos da lista
-//			.map(c -> c.getAlunos()) //map: mapeia um tipo de objeto (ex.: curso) para outro (Integer do getAlunos)
+//			.map(c -> c.getAlunos()) //map: mapeia um tipo de objeto (ex.: curso) para outro tipo (Integer do getAlunos)
 //			.mapToInt(Curso::getAlunos) //mapToInt: possui operações internas como o .sum() para somar ints
 //			.sum(); // sum: operação do mapToInt para somar os valores inteiros de cada objeto
 //			.forEach(c -> System.out.println(c.getAlunos()));
@@ -22,16 +25,43 @@ public class ExemploCursos {
 		/*
 		 * new tests in stream api
 		 */
-		cursos.stream()
-			.filter(c -> c.getAlunos() >= 50)
-			.forEach(c -> System.out.println(c.getNome()));
-		
-		cursos.stream()
-			.map(Curso::getNome);
+//		cursos.stream()
+//			.filter(c -> c.getAlunos() >= 50)
+//			.forEach(c -> System.out.println(c.getNome()));
+//		
+//		cursos.stream()
+//			.map(Curso::getNome); //mapeando o tipo de objeto curso para o tipo string (getNome)
 		// collect: pesquisar
 		// findFirst: pesquisar
-		// findAny: pesquisar
 		
+		/*
+		 * Classe Optional: nova classe do java 8 que ajuda a trabalhar com objetos nulos.
+		 */
+//		Optional<Curso> optionalCurso = cursos.stream()
+//			.filter(c -> c.getAlunos() >= 100) // filtra todos os cursos da lista a partir do predicate
+//			.findAny(); // retorna um Optional<Curso> com qualquer curso (aleatório).
+		
+//		Curso curso = optionalCurso.get(); // retorna o objeto original dentro do optional (pode retornar null)
+//		Curso curso2 = optionalCurso.orElse(new Curso("Padrao", 0)); // retorna um objeto se não tiver, retorna o que foi passado por parametro
+//		optionalCurso.ifPresent(c -> System.out.println(c.getNome())); //ifPresente do Optional se existir ele executa o lambda que imprime
+		//o nome do curso, se não tiver nem um curso ele não faz nada (não lança exception).
+		
+		/*
+		 * Forma mais comum de usar Optional, stream e lambda
+		 */
+//		cursos.stream()
+//			.filter(c -> c.getAlunos() >= 100) // filtar os elementos
+//			.findAny() // pega um elemento qualquer
+//			.ifPresent(c -> System.out.println(c.getNome())); // se existir um elemento printa o nome
+		
+//		cursos = cursos.stream()
+//			.filter(c -> c.getAlunos() >= 100)
+//			.collect(Collectors.toList()); // transforma a stream filtrada e converte para uma List<Cursos>
+		
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.collect(Collectors.toMap(Curso::getNome,Curso::getAlunos))// convertendo para map, tendo getNome como chave e getAlunos como valor.
+			.forEach((nome, valor) -> System.out.println(nome + " tem " + valor + " Alunos.")); 		
 	}
 
 }
